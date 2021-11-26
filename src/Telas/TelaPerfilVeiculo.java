5acb04be37dcbaa5b2efea71a85353f0696ad3e5
gabrielmdlima <gabriel.mdlima@gmail.com>
2021-11-25 23:08:45 -0300

    TelaPerfilVeiculo1.0

--- src/Telas/TelaPerfilVeiculo.java
+++ src/Telas/TelaPerfilVeiculo.java
@@ -1,4 +1,15 @@
 package Telas;
 
+import javax.swing.*;
+
 public class TelaPerfilVeiculo {
+  private JPanel mainPanel;
+  private JTextField textField1;
+  private JLabel lbPlaca;
+  private JTextField textField2;
+  private JTextField textField3;
+  private JTextField textField4;
+  private JTextField textField5;
+  private JButton button1;
+  private JButton button2;
 }
